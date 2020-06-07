e23baa8446a7ee4a5d0ce9197b4a9dde571c8c68
echenim <myron.echenim@live.com>
2020-06-07 15:09:34 -0700

    setup develop handlers

--- src/http_waiter_app.erl
+++ src/http_waiter_app.erl
@@ -9,18 +9,19 @@
 
 -export([start/2, stop/1]).
 
-start(_StartType, _StartArgs) ->
-   Dispatch = cowboy_router:compile([
+start(_StartType, _StartArgs) ->   
+    Dispatch = cowboy_router:compile([
 		{'_', [
-			{"/", root_handler, []}
+			{"/", cowboy_static, {priv_file, http_waiter, "index.html"}},
+			{"/upload", http_upload_handler, []}
 		]}
 	]),
 	{ok, _} = cowboy:start_clear(http, [{port, 8083}], #{
 		env => #{dispatch => Dispatch}
-	}),
+	}),	
     http_waiter_sup:start_link().
 
-stop(_State) ->
+stop(_State) ->   
     ok = cowboy:stop_listener(http).
 
 %% internal functions
